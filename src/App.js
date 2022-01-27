c1d8b547b01e01b16d1a278be8c9d8c87ea2a09e
leyksnal <leyksnal@gmail.com>
2022-01-27 17:17:19 +0100

    hero boxes placed

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ function App() {
     <div className="main">
       <div className="container">
         <div className="box1">
-          <button className='tag'>CRM</button>
-          <h2>Unite sales and marketing around the customer with an all-in-one CRM</h2>
-          <p>Know customer intent to help sales convert faster with a 360° view</p>
-          <div className="call-to-acction">
-            <button>Sign up</button><button>View pricing</button>
+          <button className='tag'><span>CRM</span></button>
+          <h2 className='title'>Unite sales and marketing around the customer with an all-in-one CRM</h2>
+          <p className='description'>Know customer intent to help sales convert faster with a 360° view</p>
+          <div className="call-actions">
+            <button className='action1'>Sign up</button>
+            <button className='action2'>View pricing</button>
           </div>
         </div>
         <div className="box2"></div>
