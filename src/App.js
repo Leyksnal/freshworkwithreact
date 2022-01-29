13fb2be0407d538ab6c31576fe4e55285575c209
leyksnal <leyksnal@gmail.com>
2022-01-29 17:11:42 +0100

    header info opportunity section

--- src/App.js
+++ src/App.js
@@ -3,17 +3,69 @@ import './App.css';
 function App() {
   return (
     <div className="main">
+      <div className="holder">
+        <div className="inner">
+          <div className="logo">
+            <div className="logoimg"></div>
+            <div className="text">Freshsales Suite</div>
+          </div>
+          <div className="nav-btn">
+            <span>Features</span><span>Pricing</span><span>Resources</span><span>Industries</span><span>Login</span>
+            <button className='action'>SIGN UP</button>
+          </div>
+        </div>
+      </div>
       <div className="container">
         <div className="box1">
           <button className='tag'><span>CRM</span></button>
           <h2 className='title'>Unite sales and marketing around the customer with an all-in-one CRM</h2>
           <p className='description'>Know customer intent to help sales convert faster with a 360° view</p>
           <div className="call-actions">
-            <button className='action1'>Sign up</button>
-            <button className='action2'>View pricing</button>
+            <button className='action1'>SIGN UP</button>
+            <button className='action2'>VIEW PRICING</button>
           </div>
         </div>
         <div className="box2"></div>
+      </div>
+
+      <div className="info-section">
+        <div className="wrapper">
+          <h2 className='heading'>How can Freshsales Suite help you grow your business?</h2>
+          <div className="card-container">
+            <div className="inner-card">
+              <div className="cardimg1"></div>
+              <p className="cardheading">Nurture relationships and grow revenue with a single customer view </p>
+              <p className="cardsubheading">Deliver seamless engagement across channels with a 360° view of the customer. Unify sales and marketing teams to deliver customer delight.</p>
+            </div>
+            <div className="inner-card">
+              <div className="cardimg2"></div>
+              <p className="cardheading">Close deals faster by sending the right message at the right time</p>
+              <p className="cardsubheading">Sales and marketing can work together to deliver personalized customer experiences. Understand customer intent and send the right message to close deals faster.</p>
+            </div>
+            <div className="inner-card">
+              <div className="cardimg3"></div>
+              <p className="cardheading">Reduce IT complexity & cost with a comprehensive solution</p>
+              <p className="cardsubheading">Keep teams happy with a single, all-in-one solution that is affordable, easy to implement, and easier to use. Increase adoption, reduce costs and boost morale.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="opporunity-section">
+        <div className="wrapper1">
+          <div className="section-img"></div>
+          <div className="opportunity-info">
+            <button className="opportunity-tag">FIND MORE OPPORTUNITIES</button>
+            <h2 className="opportunity-heading">Attract the best leads. Generate 10X more opportunities.</h2>
+            <p className="opportunity-subheading">Attract visitors, convert them into leads, run campaigns, and turn your website into a lead generation machine.</p>
+            <h3 className="features">Top features:</h3>
+            <div className="todo-wrapper">
+              <div className="todoimg"></div>
+              <div className="todotext">Create landing pages in minutes. Monitor and understand visitor intent.</div>
+            </div>
+            <span className="more">Learn more {'->'}</span>
+          </div>
+        </div>
       </div>
     </div>
   );
